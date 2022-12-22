06c1feb8a4a043d81d0f706316ea7577349ceea0
Rafael Ortiz <rafael007@gmail.com>
2022-12-22 07:52:01 -0500

    Manejo de strings - parte1

---
+++ 04-strings.py
@@ -0,0 +1,9 @@
+my_name = "Rafael"
+
+
+# String concatenation and strings in two lines
+print(my_name + "\nString length: " + str(len(my_name)))
+
+# Tab
+my_lastname = "Ortiz"
+print(my_lastname + " (here comes a tab)\tString length: " + str(len(my_lastname)))
