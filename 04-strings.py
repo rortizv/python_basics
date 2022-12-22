93b06c61cfad971159ca0989d865ed841f20d761
Rafael Ortiz <rafael007@gmail.com>
2022-12-22 08:06:32 -0500

    String formatting in Python3

--- 04-strings.py
+++ 04-strings.py
@@ -1,9 +1,12 @@
 my_name = "Rafael"
 
-
 # String concatenation and strings in two lines
 print(my_name + "\nString length: " + str(len(my_name)))
 
 # Tab
 my_lastname = "Ortiz"
 print(my_lastname + " (here comes a tab)\tString length: " + str(len(my_lastname)))
+
+# Formatted string - this is the correct way to format strings in Python 3
+name, lastname, age = "Rafael", "Ortiz", 38
+print(f"My name is {name} {lastname} and I'm {age} years old")
